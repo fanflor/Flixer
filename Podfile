c7139583852d8745306ae79129c3966026851605
fanflor <51783444+fanflor@users.noreply.github.com>
2020-09-18 23:54:12 -0400

    Add files via upload

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'flixer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flixer
+pod 'Alamofire', '~> 4.4'
+pod 'AlamofireImage'
+pod 'OAuthSwift', '~> 1.1.0'
+pod 'OAuthSwiftAlamofire'
+
+end
+
+
+
---
+++ flixer/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  flixer
+//
+//  Created by searto  on 9/15/20.
+//  Copyright © 2020 Fanflor theodore. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
